1acba223b9330b04fd2ccea728315d1c9bef8ddf
handar423 <yunshanjia423@gmail.com>
2023-10-23 21:28:44 +0800

    nits

--- scripts/image_setup.bash
+++ scripts/image_setup.bash
@@ -1,7 +1,7 @@
 #git pull
-docker build --no-cache -t  codeless_base ../src/container
-bash ../benchmark/svd/create_image.sh
-bash ../benchmark/video/create_image.sh
-bash ../benchmark/wordcount/create_image.sh
-bash ../benchmark/recognizer/create_image.sh
+docker build --no-cache -t  codeless_base /mydata/FaaSPlace/src/container
+bash /mydata/FaaSPlace/benchmark/svd/create_image.sh
+bash /mydata/FaaSPlace/benchmark/video/create_image.sh
+bash /mydata/FaaSPlace/benchmark/wordcount/create_image.sh
+bash /mydata/FaaSPlace/benchmark/recognizer/create_image.sh
 docker image prune -f
